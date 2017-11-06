40198188c6bfb6ff961848715d33ca7b0d848208
Javier Jimenez and Senya Webster <senya@>
2017-11-06 15:36:32 -0800

    remove firebase gitignore

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+    apiKey: "AIzaSyDMoPOJUUbV6HPozrRGb_W2Bhea11pttUg",
+    authDomain: "spotifyapp-ced4c.firebaseapp.com",
+    databaseURL: "https://spotifyapp-ced4c.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "1056466263"
+  };
