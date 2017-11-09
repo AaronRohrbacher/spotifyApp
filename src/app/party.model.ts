5cbe13a867b6d5a85cdd74e17e665fb54b970a93
Aaron Rohrbacher <arjazz@icloud.com>
2017-11-09 10:22:42 -0800

    fiix javi

--- src/app/party.model.ts
+++ src/app/party.model.ts
@@ -4,6 +4,6 @@ export class Party {
     public date: string,
     public location: string,
     public danceability: string,
-    public playlist: object[] = []
+    public playlist: string[] = []
   ) {}
 }
